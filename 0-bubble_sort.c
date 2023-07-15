7d6e12dce67b571109ade76f72b40f65908d2d49
pascallino <luvpascal.ojukwu@yahoo.com>
2023-07-14 23:02:50 -0700

    1-O

---
+++ 0-bubble_sort.c
@@ -0,0 +1,52 @@
+#include "sort.h"
+
+/**
+ * swap_func - swap numbers
+ * @idx1: first num
+ * @idx2: second num
+ * @array: array to be swapped in memmory
+ * Return: void
+ */
+
+void swap_func(int *array, int idx1, int idx2)
+{
+int *ptr = array;
+int temp;
+
+temp = ptr[idx1];
+ptr[idx1] = ptr[idx2];
+ptr[idx2] = temp;
+}
+
+/**
+ * bubble_sort -  bubble sort numbers
+ * @size: array size
+ * @array: array to be swapped in memmory
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+int i, len = 0;
+int *A = array;
+bool swapped = false;
+
+len = size;
+if (len == 1)
+	return;
+do {
+swapped = false;
+for (i = 1; i < len; i++)
+{
+if (A[i - 1] > A[i])
+{
+	swap_func(A, i - 1, i);
+	swapped = true;
+	print_array(A, len);
+}
+}
+
+} while (swapped == true);
+
+
+
+}
--- sort.h
+++ sort.h
@@ -1,3 +1,23 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stddef.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -11,3 +31,10 @@ const int n;
 struct listint_s *prev;
 struct listint_s *next;
 } listint_t;
+
+void swap_func(int *array, int idx1, int idx2);
+void bubble_sort(int *array, size_t size);
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+
+#endif
