36ca64cb04b43dba156363e5a95aca60a2e5a32b
pascallino <luvpascal.ojukwu@yahoo.com>
2023-07-17 15:14:45 +0100

    testdeck.c

--- testdeck.c
+++ testdeck.c
@@ -1,130 +1,112 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "deck.h"
 
-typedef enum kind_e {
-    SPADE = 0,
-    HEART,
-    CLUB,
-    DIAMOND
-} kind_t;
+int _strcmp(const char *s1, const char *s2);
+char get_value(deck_node_t *card);
+void insertion_sort_deck_kind(deck_node_t **deck);
+void insertion_sort_deck_value(deck_node_t **deck);
+void sort_deck(deck_node_t **deck);
 
-typedef struct card_s {
-    const char *value;
-    const kind_t kind;
-} card_t;
-
-typedef struct deck_node_s {
-    const card_t *card;
-    struct deck_node_s *prev;
-    struct deck_node_s *next;
-} deck_node_t;
-
-// Function to compare two cards for sorting
-int compare_cards(const void *a, const void *b) {
-    const card_t *card_a = *((const card_t **)a);
-    const card_t *card_b = *((const card_t **)b);
-
-    if (card_a->kind < card_b->kind)
-        return -1;
-    else if (card_a->kind > card_b->kind)
-        return 1;
-
-    // If the cards have the same kind, compare their values
-    // Assume the values are in the order of "Ace" to "King"
-    const char *values[] = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
-
-    int value_a, value_b;
-
-    for (int i = 0; i < 13; i++) {
-        if (card_a->value == values[i])
-            value_a = i;
-        if (card_b->value == values[i])
-            value_b = i;
+int _strcmp(const char *s1, const char *s2)
+{
+    while (*s1 && *s2 && *s1 == *s2)
+    {
+        s1++;
+        s2++;
     }
 
-    return value_a - value_b;
+    if (*s1 != *s2)
+        return (*s1 - *s2);
+    return (0);
 }
 
-// Function to sort a deck of cards
-void sort_deck(deck_node_t **deck) {
-    // Count the number of cards in the deck
-    int count = 0;
-    deck_node_t *current = *deck;
-
-    while (current != NULL) {
-        count++;
-        current = current->next;
-    }
-
-    // Create an array of card pointers
-    card_t *cards[count];
-    current = *deck;
-
-    for (int i = 0; i < count; i++) {
-        cards[i] = (card_t *)current->card;
-        current = current->next;
-    }
-
-    // Sort the array of card pointers using qsort
-    qsort(cards, count, sizeof(card_t *), compare_cards);
-
-    // Reorder the deck using the sorted array of card pointers
-    current = *deck;
-
-    for (int i = 0; i < count; i++) {
-        current->card = cards[i];
-        current = current->next;
-    }
+char get_value(deck_node_t *card)
+{
+    if (_strcmp(card->card->value, "Ace") == 0)
+        return (0);
+    if (_strcmp(card->card->value, "1") == 0)
+        return (1);
+    if (_strcmp(card->card->value, "2") == 0)
+        return (2);
+    if (_strcmp(card->card->value, "3") == 0)
+        return (3);
+    if (_strcmp(card->card->value, "4") == 0)
+        return (4);
+    if (_strcmp(card->card->value, "5") == 0)
+        return (5);
+    if (_strcmp(card->card->value, "6") == 0)
+        return (6);
+    if (_strcmp(card->card->value, "7") == 0)
+        return (7);
+    if (_strcmp(card->card->value, "8") == 0)
+        return (8);
+    if (_strcmp(card->card->value, "9") == 0)
+        return (9);
+    if (_strcmp(card->card->value, "10") == 0)
+        return (10);
+    if (_strcmp(card->card->value, "Jack") == 0)
+        return (11);
+    if (_strcmp(card->card->value, "Queen") == 0)
+        return (12);
+    return (13);
 }
 
-// Function to print the sorted deck
-void print_sorted_deck(deck_node_t **deck) {
-    deck_node_t *current = *deck;
+void insertion_sort_deck_kind(deck_node_t **deck)
+{
+    deck_node_t *iter, *insert, *tmp;
 
-    while (current != NULL) {
-        printf("Value: %s, Kind: %d\n", current->card->value, current->card->kind);
-        current = current->next;
+    for (iter = (*deck)->next; iter != NULL; iter = tmp)
+    {
+        tmp = iter->next;
+        insert = iter->prev;
+        while (insert != NULL && insert->card->kind > iter->card->kind)
+        {
+            insert->next = iter->next;
+            if (iter->next != NULL)
+                iter->next->prev = insert;
+            iter->prev = insert->prev;
+            iter->next = insert;
+            if (insert->prev != NULL)
+                insert->prev->next = iter;
+            else
+                *deck = iter;
+            insert->prev = iter;
+            insert = iter->prev;
+        }
     }
 }
 
-// Example usage
-int main() {
-    // Create a deck of cards (already in a shuffled order)
-    card_t cards[] = {
-        {"Ace", SPADE},
-        {"King", CLUB},
-        {"8", DIAMOND},
-        {"4", HEART},
-        {"Queen", DIAMOND},
-        {"10", CLUB},
-        // Add the remaining cards...
-    };
+void insertion_sort_deck_value(deck_node_t **deck)
+{
+    deck_node_t *iter, *insert, *tmp;
 
-    // Create a doubly linked list to represent the deck
-    deck_node_t *deck = NULL;
-    deck_node_t *prev = NULL;
-
-    for (int i = 0; i < 52; i++) {
-        deck_node_t *node = malloc(sizeof(deck_node_t));
-        node->card = &cards[i];
-        node->prev = prev;
-        node->next = NULL;
-
-        if (prev != NULL)
-            prev->next = node;
-
-        prev = node;
-
-        if (deck == NULL)
-            deck = node;
+    for (iter = (*deck)->next; iter != NULL; iter = tmp)
+    {
+        tmp = iter->next;
+        insert = iter->prev;
+        while (insert != NULL &&
+               insert->card->kind == iter->card->kind &&
+               get_value(insert) > get_value(iter))
+        {
+            insert->next = iter->next;
+            if (iter->next != NULL)
+                iter->next->prev = insert;
+            iter->prev = insert->prev;
+            iter->next = insert;
+            if (insert->prev != NULL)
+                insert->prev->next = iter;
+            else
+                *deck = iter;
+            insert->prev = iter;
+            insert = iter->prev;
+        }
     }
+}
 
-    // Sort the deck
-    sort_deck(&deck);
+void sort_deck(deck_node_t **deck)
+{
+    if (deck == NULL || *deck == NULL || (*deck)->next == NULL)
+        return;
 
-    // Print the sorted deck
-    printf("Sorted deck:\n");
-    print_sorted_deck(&deck);
-
-    return 0;
+    insertion_sort_deck_kind(deck);
+    insertion_sort_deck_value(deck);
 }
